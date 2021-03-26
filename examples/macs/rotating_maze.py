0d2c23670a49fb61d2484015f65e080ce479351f
Norbert Kozlowski <norbert.kozlowski@arcsoftware.it>
2021-03-26 16:06:31 +0100

    Added experiment for MACS

--- examples/macs/rotating_maze.py
+++ examples/macs/rotating_maze.py
@@ -1,17 +1,50 @@
+import logging
+
 import gym
 # noinspection PyUnresolvedReferences
 import gym_maze
-import logging
+from lcs import Perception
 from lcs.agents.macs.macs import Configuration, MACS
 
 logging.basicConfig(level=logging.INFO)
 
 
-def _metrics(agent, env):
-    pop_len = len(agent.population)
+def _calculate_knowledge(agent: MACS, env):
+    transitions = env.env.transitions
+    covered_transitions = 0
+
+    debug = dict()
+    debug[0] = set()
+    debug[1] = set()
+    debug[2] = set()
+
+    for p0, a, p1 in transitions:
+        p0p = Perception(list(map(str, p0)))
+        p1p = Perception(list(map(str, p1)))
+        anticipations = list(agent.get_anticipations(p0p, a))
+
+        # accurate classifiers
+        if len(anticipations) == 1 and anticipations[0] == p1p:
+            debug[a].add(p0p)
+            covered_transitions += 1
+
     return {
-        'pop': pop_len,
-        'accurate': len([cl for cl in agent.population if cl.is_accurate]),
+        'score': covered_transitions / len(transitions),
+        'good_0_actions': len(debug[0]),
+        'good_1_actions': len(debug[1]),
+        'good_2_actions': len(debug[2]),
+    }
+
+
+def _metrics(agent: MACS, env):
+    population = agent.population
+    return {
+        'pop': len(population),
+        'situations': len(agent.desirability_values),
+        '0_cls': len([cl for cl in population if cl.action == 0 and cl.is_accurate]),
+        '1_cls': len([cl for cl in population if cl.action == 1 and cl.is_accurate]),
+        '2_cls': len([cl for cl in population if cl.action == 2 and cl.is_accurate]),
+        'knowledge': _calculate_knowledge(agent, env)
     }
 
 
@@ -23,13 +56,13 @@ if __name__ == '__main__':
     cfg = Configuration(classifier_length=8,
                         number_of_possible_actions=3,
                         feature_possible_values=[state_values] * 8,
-                        metrics_trial_frequency=1,
+                        metrics_trial_frequency=10,
                         user_metrics_collector_fcn=_metrics)
 
     agent = MACS(cfg)
 
     print("\n*** EXPLORE ***")
-    pop, metrics = agent.explore(env, 100)
+    pop, metrics = agent.explore(env, 1000)
 
     for cl in pop:
         print(cl)
